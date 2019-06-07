0fe732e91f3310f76d7eb4657a4c0e3fa0e851ca
domtom1126 <domtom1126@gmail.com>
2019-06-07 06:12:06 -0700

    Added Main Menu

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -6,9 +6,35 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
-            Game();
+            Console.Clear();
+            MainMenu();
         }
 
+        private static void MainMenu()
+        {
+            Console.WriteLine("Welcome to Blackjack!");
+            Console.WriteLine("1) Play");
+            Console.WriteLine("2) Rules");
+            Console.WriteLine("3) Exit");
+            int menuSelect = Int32.Parse(Console.ReadLine());
+            if (menuSelect == 1)
+            {
+                Game();
+                MainMenu();
+            }
+            else if (menuSelect == 2)
+            {
+                Console.WriteLine("The game of blackjack is played where you try to get as close as possible to 21 without going over" +
+                    "If you go over the dealer wins, if the dealer goes over you win, if the dealer gets closer to 21 you lose," +
+                    "if you are closer to 21 than the dealer, you win.");
+                MainMenu();
+            }
+            else
+            {
+                MainMenu();
+            }
+            
+        }
         static void Game()
         {
             // Dealer Cards
@@ -20,10 +46,7 @@ namespace Blackjack
             int dealerSecondCard = rand.Next(dealerCards.Length);
             //Adds first and second card to make dealerHand
             int dealerHand = dealerFirstCard + dealerSecondCard;
-            if (dealerHand <= 16)
-            {
-                dealerHand = dealerHand += dealerFirstCard;
-            }
+
 
 
             //Users Cards
